b68cff91c42b5756a6559432673e4259c4b7c2d7
Victor <vicads01@gmail.com>
2025-08-08 16:10:14 +0100

    Update binary search

--- dsa/arrays/searching.py
+++ dsa/arrays/searching.py
@@ -1,6 +1,7 @@
 from typing import Any, List
 
-array = arr = [3, 7, 2, 9, 5]
+array = [2, 3, 5, 7, 9]
+array2 = [-1, 0, 3, 5, 9, 12]
 
 
 def linear_search(arr: List[Any], search_term:Any)->int:
@@ -17,22 +18,22 @@ def binary_search(arr: List[Any], search_term: Any)->int:
     So the effective time complexity is the that of the sorting algorithm
     """
     low = 0
-    high = len(arr)-1
-    
-    while low<=high:
-        mid = (high + low)//2
-        if search_term == mid:
+    high = len(arr) - 1
+
+    while low <= high:
+        mid = (high + low) // 2
+        if search_term == arr[mid]:
             return mid
-        if search_term < mid:
-            high = mid
+        elif search_term < arr[mid]:
+            high = mid - 1
         else:
-            low=mid
+            low = mid + 1
     return -1
 
 print(f'Linear search for 3 in {array}: {linear_search(array, 3)}')
 print(f'Linear search for 4 in {array}: {linear_search(array, 4)}')
 print(f'Linear search for 9 in {array}: {linear_search(array, 9)}')
 
-print(f'Binary search for 3 in {array}: {linear_search(array, 3)}')
-print(f'Binary search for 4 in {array}: {linear_search(array, 4)}')
-print(f'Binary search for 9 in {array}: {linear_search(array, 9)}')
+print(f"Binary search for 3 in {array}: {binary_search(array, 3)}")
+print(f"Binary search for 4 in {array}: {binary_search(array, 4)}")
+print(f"Binary search for 9 in {array}: {binary_search(array, 9)}")
