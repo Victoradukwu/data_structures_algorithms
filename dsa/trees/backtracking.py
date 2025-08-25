25b829483832adc4e136e5d8e0efa908b056aa36
Victor <vicads01@gmail.com>
2025-08-25 19:04:27 +0100

    Update

--- dsa/trees/backtracking.py
+++ dsa/trees/backtracking.py
@@ -7,27 +7,27 @@ from bst import TreeNode
 class Backtracking:
     
     def can_reach_root(self, root: Optional[TreeNode])-> bool:
-        """_summary_
+        """_Neetcode_
 
         Given a binary tree, determine if a path exists from the root to any leaf node, without crossing a node whose value is zero
-        
+
         Time complexity is O(n), since we will touch every single node
         """
         if not root or root.val == 0:
             return False
-        
-        if not (root.left or root.right): # Successfully got to leaf node without passing a zero
+
+        if not (root.left or root.right):  # Successfully got to leaf node without passing a zero
             return True
-        
+
         if self.can_reach_root(root.left):
             return True
-        
+
         if self.can_reach_root(root.right):
             return True
         return False
-    
-    def leafPath(self, root, path):
-        """_summary_
+
+    def leaf_path(self, root, path):
+        """_Neetcode_
 
         Check if a path exists from the root node to a leaf node, without a zero. Determine such a path
         """
@@ -37,19 +37,20 @@ class Backtracking:
 
         if not root.left and not root.right:
             return True
-        if self.leafPath(root.left, path):
+        if self.leaf_path(root.left, path):
             return True
-        if self.leafPath(root.right, path):
+        if self.leaf_path(root.right, path):
             return True
         path.pop()
         return False
-    
-    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+
+    def path_sum(self, root: Optional[TreeNode], targetSum: int) -> bool:
         """_Leetcode_Easy_
 
         Given the root of a binary tree and an integer `targetSum`, return `True` if the tree has a root-to-leaf path such that adding up all the values along the path equals `targetSum`.
         """
-        def dfs(node, curr_sum):
+
+        def dfs(node, curr_sum: int) -> bool:
             if not node:
                 return False
 
