4e7045159c7188045a4a39f8baf64b4ab4b7e13d
Victor <vicads01@gmail.com>
2025-08-28 03:02:04 +0100

    Update to Priority Queues

--- dsa/misc/priority_queue.py
+++ dsa/misc/priority_queue.py
@@ -5,42 +5,51 @@ from typing import List
 class PriorityQueue:
     """
     The advantage of Priority Queue over BST is that the min/max can be obtaied in time complexity  O(1)
-    Anothr one is that building BST is time complexity of O(nlogn), but for Priorit Queue, it is O(n)
-    
+    Anothr one is that building BST is time complexity of O(nlogn), but for Priorit Queue, it is O(n)--heapify
+
     The downside is that search is O(n), unlike bst where it is O(logn)
+    Note: The Priority Queue can be used to sort an array: heapify, and then pop each item, giving a time complexity of O(nlogn)
+    Note: Priority Queue is not good for searching
     """
     def __init__(self) -> None:
         self.heap = []
         
     def push(self, val):
         self.heap.append(val)
-        i = len(self.heap) - 1 # This assumes a 1-based index, meaning there is a always a value 0 at index 0, not evident in the array. [7, 9] is considered to have a length of 3, ie [0,7 ,9] 
+        i = (
+            len(self.heap) - 1
+        )  # i is the index of the recently added value. This assumes that the first item in the array is at index 1, with a 0 at index 0
 
         # Percolate up
         while i > 1 and self.heap[i] < self.heap[i // 2]:
             self.heap[i], self.heap[i//2] = self.heap[i//2], self.heap[i] # Swap
             i = i // 2
-    
+
     def pop(self):
+        """
+        Time Complexity: O(logn)
+        """
         if len(self.heap) == 1:
             return None
         if len(self.heap) == 2:
             return self.heap.pop()
 
-        res = self.heap[1]   
-        # Move last value to root
+        res = self.heap[1]  # save the return value, csince we're going to overwrite it soon
+
+        # Move last value to root. This is to allow us percolate down, since percolate down is more efficient than percolate up
         self.heap[1] = self.heap.pop()
         i = 1
         # Percolate down
         while 2 * i < len(self.heap):
-            if (2 * i + 1 < len(self.heap) and 
-            self.heap[2 * i + 1] < self.heap[2 * i] and 
-            self.heap[i] > self.heap[2 * i + 1]): # if there is a right child and the right child is less than both the node and left child
+            if (
+                2 * i + 1 < len(self.heap)
+                and self.heap[2 * i + 1] < self.heap[2 * i]
+                and self.heap[2 * i + 1] < self.heap[i]
+            ):  # if there is a right child and the right child is less than both the node and left child
                 # Swap right child
                 self.heap[i], self.heap[2*i+1] = self.heap[2*i+1], self.heap[i]
                 i = 2 * i + 1
-            elif self.heap[i] > self.heap[2 * i]:
-                # Swap left child
+            elif self.heap[i] > self.heap[2 * i]:  # The left child is less than the node, so swap left child
                 self.heap[i], self.heap[2 * i] = self.heap[2 * i], self.heap[i]
                 i = 2 * i
             else:
@@ -48,28 +57,30 @@ class PriorityQueue:
         return res
     
     def heapify(self, arr: List[int])->None:
-    # 0-th position is moved to the end
+        """
+        Time Complexity: O(n)
+        """
+        # 0-th position is moved to the end, so that the oth position can be ignored
         arr.append(arr[0])
 
         self.heap = arr
+        # The last half of the list have no children, so, no need to check for a possible swap. The check (percolate down) is only done for the first half of the list
         cur = (len(self.heap) - 1) // 2
-        while cur > 0:
+        while cur > 0:  # Real data starts from index 1, not index 0
             # Percolate down
             i = cur
             while 2 * i < len(self.heap):
-                if (2 * i + 1 < len(self.heap) and 
-                self.heap[2 * i + 1] < self.heap[2 * i] and 
-                self.heap[i] > self.heap[2 * i + 1]):
+                if (
+                    2 * i + 1 < len(self.heap)
+                    and self.heap[2 * i + 1] < self.heap[2 * i]
+                    and self.heap[2 * i + 1] < self.heap[i]
+                ):
                     # Swap right child
-                    tmp = self.heap[i]
-                    self.heap[i] = self.heap[2 * i + 1]
-                    self.heap[2 * i + 1] = tmp
+                    self.heap[i], self.heap[2 * 1 + i] = self.heap[2 * i + 1], self.heap[i]
                     i = 2 * i + 1
                 elif self.heap[i] > self.heap[2 * i]:
                     # Swap left child
-                    tmp = self.heap[i]
-                    self.heap[i] = self.heap[2 * i]
-                    self.heap[2 * i] = tmp
+                    self.heap[i], self.heap[2 * i] = self.heap[2 * i], self.heap[i]
                     i = 2 * i
                 else:
                     break
@@ -100,23 +111,24 @@ class KthLargest:
 def lastStoneWeight(stones: List[int]) -> int:
     """_Neetcode_Easy_
 
-    You are given an array of integers stones where stones[i] represents the weight of the ith stone.
+    You are given an array of integers `stones` where `stones[i]` represents the weight of the `ith` stone.
     We want to run a simulation on the stones as follows:
 
-    At each step we choose the two heaviest stones, with weight x and y and smash them togethers
+    At each step we choose the two heaviest stones, with weight `x` and `y` and smash them together
     If x == y, both stones are destroyed
     If x < y, the stone of weight x is destroyed, and the stone of weight y has new weight y - x.
     Continue the simulation until there is no more than one stone remaining.
 
     Return the weight of the last remaining stone or return 0 if none remain.
     """
+    import heapq
+    
     if not stones:
         return 0
     if len(stones) == 1:
         return stones[0]
-    import heapq
 
-    heap = [-x for x in stones]
+    heap = [-x for x in stones] # Create a max-heap, since a pop should return the largest element
     heapq.heapify(heap)
 
     while len(heap) > 1:
@@ -144,5 +156,11 @@ def kClosest(points: List[List[int]], k: int) -> List[List[int]]:
     return sorted_points[:k]
 
 def findKthLargest(nums: List[int], k: int) -> int:
-    return sorted(nums, reverse=True)[k - 1]
+    """_Neetcode_Medium_
+
+    Given an unsorted array of integers nums and an integer k, return the kth largest element in the array.
+    By kth largest element, we mean the kth largest element in the sorted order, not the kth distinct element.
+    """
+    # return sorted(nums, reverse=True)[k - 1]. #An alternative implementation
+    return heapq.nlargest(k, nums)[-1]
 
